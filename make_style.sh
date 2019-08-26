cb43560cc8914a728a4c733cff8f57f2ea4a3696
Ozan Akın <ozn.akn@gmail.com>
2019-08-27 01:14:56 +0300

    Improved make_style.sh

--- make_style.sh
+++ make_style.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd `dirname $0`
-sass --update resources:sass_processed
-pleeease compile sass_processed/style.css -t resources/style.css
-mv sass_processed/content-description.css resources/content-description.css
-mv sass_processed/table.css resources/table.css
-mv sass_processed/ranks.css resources/ranks.css
+mkdir -p static && \
+sass --update resources:sass_processed && \
+pleeease compile sass_processed/style.css -t static/style.css && \
+mv sass_processed/content-description.css static/content-description.css && \
+mv sass_processed/table.css static/table.css && \
+mv sass_processed/ranks.css static/ranks.css && \
+find static -name "*.scss" -delete
