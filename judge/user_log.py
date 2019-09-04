49833a5dd52f9c3edf0b5b37e0fdb77bda549d8e
Ozan Akın <ozn.akn@gmail.com>
2019-09-04 19:08:44 +0300

    Change last_ip address' header

--- judge/user_log.py
+++ judge/user_log.py
@@ -13,7 +13,10 @@ class LogUserAccessMiddleware(object):
                 not getattr(request, 'no_profile_update', False)):
             updates = {'last_access': now()}
             # Decided on using REMOTE_ADDR as nginx will translate it to the external IP that hits it.
-            if request.META.get('REMOTE_ADDR'):
+            # No, REMOTE_ADDR not working on proxies, use HTTP_X_REAL_IP
+            if request.META.get('HTTP_X_REAL_IP'):
+                updates['ip'] = request.META.get('HTTP_X_REAL_IP')
+            elif request.META.get('REMOTE_ADDR'):
                 updates['ip'] = request.META.get('REMOTE_ADDR')
             Profile.objects.filter(user_id=request.user.pk).update(**updates)
 
